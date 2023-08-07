326d7969bbb99ac78ecc8c33b03e62b99f84f14b
Prabin Nepal <prabinnepal1996@gmail.com>
2023-08-06 23:26:38 -0500

    Grouping routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
       // <p className="text-6xl">Hello AI Saas</p>
-      <Button variant="destructive"> CLick Me</Button>
+      <Button variant="destructive"> Dashboard</Button>
   )
 }
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return (
+      // <p className="text-6xl">Hello AI Saas</p>
+      <Button variant="destructive"> LandingPage</Button>
+  )
+}
