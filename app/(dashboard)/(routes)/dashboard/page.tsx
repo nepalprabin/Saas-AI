f0b78f4c8bab6356416c66c388e0b4f0f0644787
Prabin Nepal <prabinnepal1996@gmail.com>
2023-09-26 23:09:22 -0500

    Added sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
       // <p className="text-6xl">Hello AI Saas</p>
       <div>
-        <Button variant="destructive"> Dashboard</Button>
-        <UserButton  afterSignOutUrl="/"/>
+        <p>Dashboard Page (Protected)</p>
       </div>
   )
 }
