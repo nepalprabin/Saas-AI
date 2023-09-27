7119d0d89c665a0faa754fc0a3e9593f541838a3
Prabin Nepal <prabinnepal1996@gmail.com>
2023-09-26 20:35:53 -0500

    Clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
       // <p className="text-6xl">Hello AI Saas</p>
-      <Button variant="destructive"> Dashboard</Button>
+      <div>
+        <Button variant="destructive"> Dashboard</Button>
+        <UserButton  afterSignOutUrl="/"/>
+      </div>
   )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,24 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
       // <p className="text-6xl">Hello AI Saas</p>
-      <Button variant="destructive"> LandingPage</Button>
-  )
+      // <Button variant="destructive"> LandingPage</Button>
+    <div>
+        <div>
+          <h3>Landing Page</h3>
+            <Link href="/sign-in">
+              <Button>
+                Sign In
+                </Button>
+            </Link>
+            <Link href="/sign-up">
+              <Button>
+                Register
+                </Button>
+            </Link>
+        </div>
+    </div>
+  );
 }
